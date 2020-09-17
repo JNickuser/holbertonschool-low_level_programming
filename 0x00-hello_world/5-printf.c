b4959b5b111152e71818182c5047ce0be2546780
JNickuser <juanmejia0809@gmail.com>
2020-09-16 21:30:01 -0500

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - main block of program
+ *
+ *Return: Indicate the output is correct
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
