01537cd7492bd79d46217cca95d814f55496a95d
JNickuser <juanmejia0809@gmail.com>
2020-09-16 21:15:02 -0500

    Message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - main function's program
+ *
+ *Return: Code's block where is executed all
+ *program.
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
